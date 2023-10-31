20f6703835492787b908b79984c4325ab2d4411e
Bruno Pache <bruno_x86@hotmail.com>
2023-10-31 18:05:45 -0300

    modificaciones en CSS

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -2,8 +2,8 @@ import cart from "./assets/cart.svg";
 
 const CartWidget = () => {
   return (
-    <div>
-      <img src={cart} alt="cart-widget"  height="50px" />0
+    <div style={{color:'white'}}>
+      <img src={cart} alt="cart-widget"  height="50px"/> 0
     </div>
   );
 };
